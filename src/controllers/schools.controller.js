4c91a10a7a1d9a238864ca65b7ef3418086b8bb5
Santiago Perera <santiagoperera07@gmail.com>
2025-04-01 23:49:45 -0300

    Se agregó la entidad Schools

---
+++ src/controllers/schools.controller.js
@@ -0,0 +1,62 @@
+const {
+    getSchools,
+    createSchool,
+    findSchool,
+    deleteSchool,
+    updateSchool
+} = require("../models/storage");
+
+const getSchoolsController = (req,res) => {
+    console.log(req.query);
+    res.status(200).json(getSchools());
+}
+
+const getSchoolController = (req,res) => {
+    const schoolId = req.params.id;
+    const school = findSchool(schoolId);
+    if(school){
+        res.status(200).json(school);
+        return;
+    }
+    res.status(404).json({
+        message: `No se ha encontrado la escuela con id: ${schoolId}`
+    })
+}
+
+const postSchoolController = async (req,res) => {
+    const {body} = req;
+    createSchool(body.title);
+    res.status(201).json({
+        message: "Escuela creada correctamente"
+    });
+}
+
+const deleteSchoolController = (req,res) => {
+    const schoolId = req.params.id;
+    deleteSchool(schoolId);
+    res.status(200).json({
+        message: "Escuela eliminada correctamente"
+    })
+}
+
+const putSchoolController = (req,res)=>{
+    const schoolId=req.params.id;
+    const {body} = req;
+    let school = findSchool(schoolId);
+    if(school){
+        school=updateSchool(schoolId,body);
+        res.status(200).json(school);
+        return;
+    }
+    res.status(404).json({
+        message: `No se ha encontrado la escuela con id: ${schoolId}`
+    });
+}
+
+module.exports={
+    getSchoolsController,
+    getSchoolController,
+    postSchoolController,
+    putSchoolController,
+    deleteSchoolController,
+}
--- src/routes/private.router.js
+++ src/routes/private.router.js
@@ -9,13 +9,28 @@ const {
     putPublicationController,
 } = require("../controllers/publications.controller");
 
+const {
+    getSchoolsController,
+    getSchoolController,
+    postSchoolController,
+    deleteSchoolController,
+    putSchoolController,
+} = require("../controllers/schools.controller");
+
 const payloadMiddleWare = require("../middlewares/payload.middleware");
 const PublicationSchema = require("../models/schemas/publication.schema");
+const SchoolSchema = require("../models/schemas/school.schema");
 
 privateRouter.get("/publications", getPublicationsController);
 privateRouter.get("/publications/:id", getPublicationController);
 privateRouter.post("/publications", payloadMiddleWare(PublicationSchema), postPublicationController);
 privateRouter.delete("/publications/:id", deletePublicationController);
 privateRouter.put("/publications/:id", payloadMiddleWare(PublicationSchema), putPublicationController);
+
+privateRouter.get("/schools", getSchoolsController);
+privateRouter.get("/schools/:id", getSchoolController);
+privateRouter.post("/schools", payloadMiddleWare(SchoolSchema), postSchoolController);
+privateRouter.delete("/schools/:id", deleteSchoolController);
+privateRouter.put("/schools/:id", payloadMiddleWare(SchoolSchema), putSchoolController);
 
 module.exports = privateRouter;
